aa77c003b220c06ab0d9bb47752e7555e2e2f5c0
최경용 <kyeongyong.c.dbd@gmail.com>
2019-12-06 16:50:44 +0900

    리덕스 카운터, 투두

---
+++ src/moudules/todos.js
@@ -0,0 +1,50 @@
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; //인풋 값을 변경함
+const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
+const TOGGLE = 'todos/TOGGLE'; // todo체크/체크 해제함
+const REMOVE = 'todos/REMOVE'; // todo를 제거함
+
+export const changeInput = createAction(CHANGE_INPUT, input => input);
+
+let id = 3; // insert가 호출될 때마다 1씩 더해진다.
+
+export const insert = createAction(INSERT, text => ({
+  id: id++,
+  text,
+  done: false,
+}));
+
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
+
+const initialState = {
+  input: '',
+  todos: [
+    { id: 1, text: '리덕스 기초 배우기', done: true },
+    { id: 2, text: '리액트와 리덕스 사용하기', done: false },
+  ],
+};
+
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, draft => {
+        const todo = draft.todos.find(todo => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter(todo => todo.id !== id),
+    }),
+  },
+  initialState,
+);
+
+export default todos;
